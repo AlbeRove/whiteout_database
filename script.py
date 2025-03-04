baaac5f5027d3f159b8786082e185610c8ebfcc0
AlbeRove <99499004+AlbeRove@users.noreply.github.com>
2025-03-04 21:48:02 +0100

    Update script.py

--- script.py
+++ script.py
@@ -212,7 +212,8 @@ save_clicked = st.button('Save')
 ChangeButtonColour('Save', 'white', '#1C86EE')
 if save_clicked:
     save_data(active_players, banned_players, former_players)
-    upload_to_github(GITHUB_REPO)
+#    upload_to_github(GITHUB_REPO)
+    upload_dataframe_to_github(banned_players, 'banned_players.csv', GITHUB_REPO, branch="master"):
     st.success("All data has been saved!")
 
 # Add download buttons
